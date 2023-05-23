96f3ac06a890d6d6540f0cfe612f2608c8903e2c
johan <rakotovaojohan516@gmail.com>
2023-05-23 12:46:47 +0300

    switch branch to front_office

---
+++ src/app/assignments/assignment.model.ts
@@ -0,0 +1,12 @@
+export class Assignment {
+    _id!: string;
+    id!: number;
+    nom!: string;
+    dateDeRendu!: Date;
+    rendu!: boolean;
+    auteur!:String;
+    matiere!:String;
+    note?:number;
+    remarque!:String;
+}
+
