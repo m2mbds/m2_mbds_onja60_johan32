3ff5e61fc732b64a6805df9b9cac7a1a1acea025
JohanGitHubMaster <rakotovaojohan516@gmail.com>
2023-05-31 12:21:40 +0300

    ajout upload

--- routes/router.js
+++ routes/router.js
@@ -8,7 +8,7 @@ const uploadRouter = express.Router();
 const upload = multer()
 
 //const KEYFILEPATH = path.join(__dirname + "\\credentials.json")
-const KEYFILEPATH = path.join(__dirname + "\\credentials.json")
+const KEYFILEPATH = path.join(__dirname + "/credentials.json")
 
 const SCOPES = ['https://www.googleapis.com/auth/drive']
 const auth = new google.auth.GoogleAuth({
